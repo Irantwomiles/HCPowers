b3b961e5009a85dbf1115a2d2fb79efe9748b212
Shayan <shayanghofrany@gmail.com>
2017-09-30 02:06:51 -0400

    enderpearl cooldown

--- HCPowers/src/com/hcpowers/profile/SetupProfile.java
+++ HCPowers/src/com/hcpowers/profile/SetupProfile.java
@@ -1,7 +1,9 @@
 package com.hcpowers.profile;
 
+import com.hcpowers.core.Core;
 import com.hcpowers.core.utils.Utils;
-import com.hcpowers.core.utils.scoreboard.PlayerBoard;
+import com.hcpowers.core.utils.PlayerBoard;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -28,14 +30,51 @@ public class SetupProfile implements Listener {
 
         PlayerProfile profile = pm.getProfileByPlayer(player);
 
-        if(profile.getPvpprot() > 0) {
+        /*if(profile.getPvpprot() > 0) {
             pb.addLine(player, player.getScoreboard(), ChatColor.BLUE.toString() + ChatColor.BOLD + "Protection: " + ChatColor.WHITE + utils.toMMSS(profile.getPvpprot()), "pvpprotection");
+        }*/
+
+        if(profile.getDeathban() > System.currentTimeMillis()) {
+
+            if(profile.getLives() > 0) {
+
+                profile.setLives(profile.getLives() - 1);
+                profile.setDeathban(0L);
+
+                player.sendMessage(ChatColor.GOLD + "Used a life, now you have " + ChatColor.GRAY + profile.getLives() + ChatColor.GOLD + " left!");
+
+            } else {
+
+                int seconds = (int) ((profile.getDeathban() - System.currentTimeMillis()) / 1000);
+
+                Bukkit.getScheduler().scheduleSyncDelayedTask(Core.getInstance(), new Runnable() {
+                    public void run() {
+                        if(!player.hasPermission("hcpower.deathban.bypass")) {
+                            player.kickPlayer(ChatColor.RED + "You are Deathbanned for " + utils.toMMSS(seconds));
+                        } else {
+                            player.sendMessage(ChatColor.YELLOW + "Bypassed Deathban, Nerd!");
+                        }
+                    }
+                }, 1);
+
+            }
+
         }
 
     }
 
     @EventHandler
     public void onQuit(PlayerQuitEvent event) {
-        pm.saveProfile(event.getPlayer());
+
+        Player player = event.getPlayer();
+
+        Bukkit.getScheduler().scheduleSyncDelayedTask(Core.getInstance(), new Runnable() {
+
+            public void run() {
+                pm.saveProfile(player);
+            }
+
+        }, 10);
+
     }
 }
